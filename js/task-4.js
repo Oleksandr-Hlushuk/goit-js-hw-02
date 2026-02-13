3f3a1762a75844168646e000ee4a3adb7de97f50
Oleksandr_Hlushuk <olhlushuk@gmail.com>
2026-02-12 18:57:16 -0600

    fix-1 task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,15 +1,15 @@
 function getShippingCost(country) {
   switch (country) {
-    case China:
+    case 'China':
       return `Shipping to China will cost 100 credits`;
       break;
-    case Chile:
+    case 'Chile':
       return `Shipping to Chile will cost 250 credits`;
       break;
-    case Australia:
+    case 'Australia':
       return `Shipping to Australia will cost 170 credits`;
       break;
-    case Jamaica:
+    case 'Jamaica':
       return `Shipping to Jamaica will cost 120 credits`;
       break;
     default:
